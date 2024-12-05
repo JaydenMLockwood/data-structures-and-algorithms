92d4338cf15a595658c60c9a53553131ea2843f9
JaydenMLockwood <jayden.marklockwood@gmail.com>
2024-12-05 12:53:42 +1030

    added insetion sort

--- sorting-algorithms.js
+++ sorting-algorithms.js
@@ -2,41 +2,50 @@ const myArray = [9,5,6,3,4,2,1,7,8];
 
 //bubble sort
 function bubbleSort(arr) {
-    let newArray = [...arr];
-    
-    for (let i = 0; i < newArray.length - 1; i++) {
-        for (let j = 0; j < newArray.length - i - 1; j++) {
-            if (newArray[j] > newArray[j + 1]) {
-                let temp = newArray[j];
-                newArray[j] = newArray[j + 1];
-                newArray[j + 1] = temp;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    return newArray;
+    return arr;
 }
 
 //selection sort
 function selectionSort(arr){
-    let newArray = [...arr];
 
-    for (let i = 0; i < newArray.length - 1; i++){
+    for (let i = 0; i < arr.length - 1; i++){
         let currentLow = i;
-        for(let j = i+1; j < newArray.length; j++){
-            if(newArray[j] < newArray[currentLow]){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[currentLow]){
                 currentLow = j;
             }
         }
-        let temp = newArray[i];
-        newArray[i] = newArray[currentLow];
-        newArray[currentLow] = temp;
+        let temp = arr[i];
+        arr[i] = arr[currentLow];
+        arr[currentLow] = temp;
     }
-    return newArray;
+    return arr;
 }
 
 //insertion sort
 function insertionSort(arr){
-    let newArray = [...arr];
 
+    for(let i = 1; i < arr.length; i++){
+        let index = arr[i];
+        let j = i - 1;
+        while(j >= 0 && arr[j] > index){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = index;
+    }
+    return arr;
 }
+
+console.log(insertionSort(myArray));
 
